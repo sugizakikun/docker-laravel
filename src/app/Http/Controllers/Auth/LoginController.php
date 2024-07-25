434fa0113cda5191c9f8cb4c8e69e679acd101d0
sugizakikun <sugizakikun@gmail.com>
2024-07-25 15:36:50 +0900

    ログアウトをタップした時の遷移先を'/login'に変更する

--- src/app/Http/Controllers/Auth/LoginController.php
+++ src/app/Http/Controllers/Auth/LoginController.php
@@ -37,4 +37,9 @@ class LoginController extends Controller
         $this->middleware('guest')->except('logout');
         $this->middleware('auth')->only('logout');
     }
+
+    protected function loggedOut()
+    {
+       return redirect(route('login'));
+    }
 }
