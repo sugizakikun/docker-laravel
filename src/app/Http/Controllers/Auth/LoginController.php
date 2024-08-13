80459fffba422134b7cd9e9c67fc83754739dc63
sugizakikun <sugizakikun@gmail.com>
2024-08-13 10:51:27 +0900

    AWS Cognitoとの連携のためLoginControllerとUserモデルを変更

--- src/app/Http/Controllers/Auth/LoginController.php
+++ src/app/Http/Controllers/Auth/LoginController.php
@@ -3,7 +3,11 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+
+//追加
+use Ellaisys\Cognito\Auth\AuthenticatesUsers as CognitoAuthenticatesUsers;
 
 class LoginController extends Controller
 {
@@ -18,14 +22,15 @@ class LoginController extends Controller
     |
     */
 
-    use AuthenticatesUsers;
+    // use AuthenticatesUsers;
+    use CognitoAuthenticatesUsers;
 
     /**
      * Where to redirect users after login.
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = RouteServiceProvider::HOME;
 
     /**
      * Create a new controller instance.
@@ -35,11 +40,41 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
-        $this->middleware('auth')->only('logout');
     }
 
-    protected function loggedOut()
-    {
-       return redirect(route('login'));
+    public function showLoginForm() {
+        return view("auth.login");
     }
+
+    /**
+     * Authenticate User
+     *
+     * @throws \HttpException
+     *
+     * @return mixed
+     */
+    public function login(\Illuminate\Http\Request $request)
+    {
+
+        //Convert request to collection
+        $collection = collect($request->all());
+
+        //Authenticate with Cognito Package Trait (with 'web' as the auth guard)
+        if ($response = $this->attemptLogin($collection, 'web')) {
+            if ($response===true) {
+                return redirect(route('home'))->with('success', true);
+            } else if ($response===false) {
+                // If the login attempt was unsuccessful you may increment the number of attempts
+                // to login and redirect the user back to the login form. Of course, when this
+                // user surpasses their maximum number of attempts they will get locked out.
+                //
+                //$this->incrementLoginAttempts($request);
+                //
+                //$this->sendFailedLoginResponse($collection, null);
+            } else {
+                return $response;
+            } //End if
+        } //End if
+
+    } //Function ends
 }
--- src/app/Models/User.php
+++ src/app/Models/User.php
@@ -4,13 +4,19 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Database\Eloquent\Model;
+// use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Auth\Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+//追加
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 
-class User extends Authenticatable
+
+class User extends Model implements AuthenticatableContract
+
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use Authenticatable, HasApiTokens, HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
