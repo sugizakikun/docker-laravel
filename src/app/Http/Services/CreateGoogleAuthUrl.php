9cc283dbff556d860af8f6df7c2f510b3adcc90c
sugizakikun <sugizakikun@gmail.com>
2024-08-24 16:13:58 +0900

    環境によるリダイレクトURLのだしわけ

--- src/app/Http/Services/CreateGoogleAuthUrl.php
+++ src/app/Http/Services/CreateGoogleAuthUrl.php
@@ -6,11 +6,14 @@ class CreateGoogleAuthUrl
 {
     public function execute()
     {
+        $prefix = $_SERVER['HTTP_HOST'] === 'localhost:8080'
+            ? 'http://' : 'https://';
+
         $params = [
             '{domain_name}' => config('cognito.domain_name'),
             '{region}' => config('cognito.region'),
             '{client_id}' => config('cognito.app_client_id'),
-            '{redirect_url}' => 'http://localhost:8080/home'
+            '{redirect_url}' =>  $prefix . $_SERVER['HTTP_HOST'] . '/home'
         ];
 
         return str_replace(array_keys($params), array_values($params), config('cognito.google_auth_url') );
