cd497f473006abb768837c6bb98af9a7e0381832
sugizakikun <sugizakikun@gmail.com>
2024-08-24 19:29:59 +0900

    ユーザー削除ロジックの追加(削除に成功したら、ログイン画面に戻るようにする)

--- src/app/Http/Services/DestroyUser.php
+++ src/app/Http/Services/DestroyUser.php
@@ -2,7 +2,33 @@
 
 namespace App\Http\Services;
 
+use App\Models\User;
+use App\Cognito\CognitoClient;
+use Illuminate\Support\Facades\DB;
+
 class DestroyUser
 {
+    public function __construct(CognitoClient $cognitoClient)
+    {
+        $this->cognitoClient = $cognitoClient;
+    }
 
+    public function execute(User $user)
+    {
+        try {
+            DB::beginTransaction();
+            $username = $user['cognito_username'];
+            $this->cognitoClient->destroyUser($username);
+
+            (new User())->newQuery()
+                ->where('cognito_username', $username)
+                ->delete();
+
+            DB::commit();
+
+            return true;
+        } catch (\Exception $e){
+            return false;
+        }
+    }
 }
