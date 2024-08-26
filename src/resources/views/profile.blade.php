687b3f405c347fb862984ab27c10aa7613898c0a
sugizakikun <sugizakikun@gmail.com>
2024-08-27 02:33:09 +0900

    画像アップローダーをモーダルの中に移動

--- src/resources/views/profile.blade.php
+++ src/resources/views/profile.blade.php
@@ -10,28 +10,53 @@
                 </div>
 
                 <div class="card-body">
-                    <div class="position-relative">
+                    <div class="position-relative mb-3">
                         <img src="{{ $user->profile_image_key ? asset($user->profile_image_key): asset('img/profile_female.png') }}" width="200" height="200">
                         <div class="position-absolute color-bg-defaultcolor-fg-default px-2 py-1 left-0 bottom-0 ml-2 mb-2">
-                            <button class="btn btn-outline-secondary">Edit</button>
+                            <button class="btn btn-light" data-toggle="modal" data-target="#updateProfileImage">Edit</button>
                         </div>
                     </div>
-                    <h3>Name</h3>
-                    {{$user->name}}
 
-                    <h3>Email address</h3>
-                    <p>{{$user->email}}</p>
-
-                    <div class="mb-3 justify-content-center">
-                        <form method="POST" action="{{ route('profile.edit') }}" enctype="multipart/form-data">
-                            @csrf
-                            <input type="hidden" name="_method" value="PUT">
-                            <input class="form-control" type="file" name="image" id="imageInput" aria-describedby="fileHelp" aria-required="true">
-                            <div id="fileHelp" class="form-text">許可されるファイル形式: JPG, PNG, GIF, WebP, AVIF (最大1MB)</div>
-
-                            <button class="btn btn-primary mt-3 col-12" id="uploadButton" disabled>アップロード</button>
-                        </form>
+                    <div class="mb-3">
+                        <label for="basic-url" class="form-label">Name</label>
+                        <div class="input-group">
+                            <input type="text" class="form-control" id="name" value="{{$user->name}}">
+                        </div>
                     </div>
+
+                    <div class="mb-3">
+                        <label for="basic-url" class="form-label">Email Address</label>
+                        <div class="input-group">
+                            <input type="text" class="form-control" id="email" value="{{$user->email}}">
+                        </div>
+                    </div>
+
+                    <!-- Modal -->
+                    <div class="modal fade" id="updateProfileImage" tabindex="-1" role="dialog" aria-labelledby="updateProfileImage" aria-hidden="true">
+                        <div class="modal-dialog modal-dialog-centered" role="document">
+                            <div class="modal-content">
+                                <form method="POST" action="{{ route('profile.edit') }}" enctype="multipart/form-data">
+                                    @csrf
+                                    <div class="modal-header">
+                                        <h5 class="modal-title" id="exampleModalCenterTitle">プロフィール画像の変更</h5>
+                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                            <span aria-hidden="true">&times;</span>
+                                        </button>
+                                    </div>
+                                    <div class="modal-body">
+                                        <input type="hidden" name="_method" value="PUT">
+                                        <input class="form-control" type="file" name="image" id="imageInput" aria-describedby="fileHelp" aria-required="true">
+                                        <div id="fileHelp" class="form-text">許可されるファイル形式: JPG, PNG, GIF, WebP, AVIF (最大1MB)</div>
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-secondary" data-dismiss="modal">閉じる</button>
+                                        <button type="submit" class="btn btn-primary" id="uploadButton" disabled>アップロード</button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+
                 </div>
             </div>
         </div>
@@ -43,6 +68,8 @@
     document.addEventListener('DOMContentLoaded', function() {
         document.getElementById('imageInput').addEventListener('change', function() {
             const uploadButton = document.getElementById('uploadButton');
+
+            console.log(uploadButton, this.files);
             if (this.files && this.files.length > 0) {
                 const file = this.files[0];
                 const maxSize = 1024 * 1024; // 10MB
