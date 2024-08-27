8406c5578a57685d48a7582702ee399e57938730
sugizakikun <sugizakikun@gmail.com>
2024-08-27 12:58:30 +0900

    アカウント削除モーダルの追加

--- src/resources/views/profile.blade.php
+++ src/resources/views/profile.blade.php
@@ -9,7 +9,7 @@
                     {{ __('Dashboard') }}
                 </div>
 
-                <div class="card-body row">
+                <div class="card-body">
                     <div class="position-relative">
                         <img src="{{ $user->profile_image_key ? asset($user->profile_image_key): asset('img/profile_female.png') }}" width="100%" height="auto">
                         <div class="dropdown position-absolute color-bg-defaultcolor-fg-default px-2 py-1 left-0 bottom-0 ml-2 mb-2">
@@ -23,20 +23,23 @@
                         </div>
                     </div>
 
-                    <div class="py-3">
-                       <div class="mb-3">
-                            <label for="basic-url" class="form-label">Name</label>
-                            <div class="input-group">
-                                <input type="text" class="form-control" id="name" value="{{$user->name}}">
-                            </div>
+                    <div class="mb-3">
+                        <label for="name" class="form-label">Name</label>
+                        <div class="input-group">
+                            <input type="text" class="form-control" id="name" value="{{$user->name}}">
                         </div>
+                    </div>
+                    <div class="mb-3">
+                        <label for="email" class="form-label">Email Address</label>
+                        <div class="input-group">
+                            <input type="text" class="form-control" id="email" value="{{$user->email}}">
+                        </div>
+                    </div>
 
-                        <div class="mb-3">
-                            <label for="basic-url" class="form-label">Email Address</label>
-                            <div class="input-group">
-                                <input type="text" class="form-control" id="email" value="{{$user->email}}">
-                            </div>
-                        </div>
+                    <h2 class="text-danger border-bottom">Delete Account</h2>
+                    <div class="mb-3">
+                        Once you delete your account, there is no going back. Please be certain.
+                        <button class="btn btn-danger mt-3" data-toggle="modal" data-target="#deleteAccount">Delete your account</button>
                     </div>
 
                     <!-- Modal(updateProfileImage) -->
@@ -65,6 +68,7 @@
                         </div>
                     </div>
 
+                    <!-- Modal(removeProfileImage) -->
                     <div class="modal fade" id="removeProfileImage" tabindex="-1" role="dialog" aria-labelledby="removeProfileImage" aria-hidden="true">
                         <div class="modal-dialog modal-dialog-centered" role="document">
                             <div class="modal-content">
@@ -82,7 +86,51 @@
                                     </div>
                                     <div class="modal-footer">
                                         <button type="button" class="btn btn-secondary" data-dismiss="modal">閉じる</button>
-                                        <button type="submit" class="btn btn-danger" id="uploadButton">リセットする</button>
+                                        <button type="submit" class="btn btn-danger" id="resetButton">リセットする</button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- Modal(deleteAccount) -->
+                    <div class="modal fade" id="deleteAccount" tabindex="-1" role="dialog" aria-labelledby="deleteAccount" aria-hidden="true">
+                        <div class="modal-dialog modal-dialog-centered" role="document">
+                            <div class="modal-content">
+                                <form method="POST" action="{{ route('withdraw') }}" enctype="multipart/form-data">
+                                    @csrf
+                                    <div class="modal-header">
+                                        <h5 class="modal-title" id="exampleModalCenterTitle">アカウントの削除</h5>
+                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                            <span aria-hidden="true">&times;</span>
+                                        </button>
+                                    </div>
+                                    <div class="alert alert-danger d-flex align-items-center" role="alert">
+                                        <div>
+                                            This is extremely important.
+                                        </div>
+                                    </div>
+                                    <div class="modal-body">
+                                        <input type="hidden" name="_method" value="DELETE">
+                                        本当に退会しますか？
+
+                                        <hr>
+                                        <div class="mb-3">
+                                            <label for="confirm-email" class="form-label">Your email:</label>
+                                            <div class="input-group">
+                                                <input type="text" class="form-control" id="confirm-email">
+                                            </div>
+                                        </div>
+                                        <div class="mb-3">
+                                            <label for="confirm-phrase" class="form-label">To verify, "delete my account" type below:</label>
+                                            <div class="input-group">
+                                                <input type="text" class="form-control" id="confirm-phrase">
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-secondary" data-dismiss="modal">閉じる</button>
+                                        <button type="submit" class="btn btn-danger" id="deleteAccountButton" disabled>退会する</button>
                                     </div>
                                 </form>
                             </div>
@@ -100,7 +148,6 @@
         document.getElementById('imageInput').addEventListener('change', function() {
             const uploadButton = document.getElementById('uploadButton');
 
-            console.log(uploadButton, this.files);
             if (this.files && this.files.length > 0) {
                 const file = this.files[0];
                 const maxSize = 1024 * 1024; // 10MB
@@ -125,3 +172,10 @@
         });
     });
 </script>
+
+<style>
+    .alert{
+        margin-bottom: 0px !important;
+        border-radius: 0 !important;
+    }
+</style>
