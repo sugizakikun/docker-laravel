37dadf2a258959e32947b612742c2120ae0c7371
sugizakikun <sugizakikun@gmail.com>
2024-08-29 20:19:13 +0900

    UpdateProfileImage:プロフィール画像が更新されたら,更新前の画像ファイルをS3からも削除する

--- src/app/Http/Services/Profile/UpdateProfileImage.php
+++ src/app/Http/Services/Profile/UpdateProfileImage.php
@@ -7,6 +7,10 @@ use Illuminate\Support\Facades\Storage;
 
 class UpdateProfileImage
 {
+    /**
+     * @param string|null $path
+     * @return void
+     */
     public function execute(?string $path)
     {
         $randomStr = base_convert(md5(uniqid()), 16,36);
@@ -17,11 +21,26 @@ class UpdateProfileImage
         $s3Path = Storage::disk('s3')->url($fileName);
 
         $user = Auth::user();
+        $oldProfileImageKey = $user->profile_image_key;
+
         $user->profile_image_url = $s3Path;
         $user->profile_image_key = $fileName;
         $user->save();
 
+        if($oldProfileImageKey !== $fileName) {
+            $this->deleteUploadedImage($oldProfileImageKey);
+        }
+
         # S3への保存が成功したらWebサーバー上の一時ファイルを削除
         Storage::delete($path);
+    }
+
+    /**
+     * @param $oldImageKey
+     * @return void
+     */
+    public function deleteUploadedImage($oldImageKey) :void
+    {
+        Storage::disk('s3')->delete($oldImageKey);
     }
 }
