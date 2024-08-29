c0eb01951759910849b6f50670e5274a45de9bee
sugizakikun <sugizakikun@gmail.com>
2024-08-29 20:00:32 +0900

    UpdateProfileImage: S3に保存する画像ファイル名がバッティングしないよう、ランダムな文字列をファイル名とする

--- src/app/Http/Services/Profile/UpdateProfileImage.php
+++ src/app/Http/Services/Profile/UpdateProfileImage.php
@@ -9,14 +9,16 @@ class UpdateProfileImage
 {
     public function execute(?string $path)
     {
+        $randomStr = base_convert(md5(uniqid()), 16,36);
+        $fileName = $randomStr.'.png';
 
         $fileContents = Storage::get($path);
-
-        Storage::disk('s3')->put('test2.png', $fileContents);
-        $s3Path = Storage::disk('s3')->temporaryUrl('test2.png', now()->addMinutes(5));
+        Storage::disk('s3')->put($fileName, $fileContents);
+        $s3Path = Storage::disk('s3')->url($fileName);
 
         $user = Auth::user();
-        $user->profile_image_key = $s3Path;
+        $user->profile_image_url = $s3Path;
+        $user->profile_image_key = $fileName;
         $user->save();
 
         # S3への保存が成功したらWebサーバー上の一時ファイルを削除
