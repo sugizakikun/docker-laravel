68d57e607b1f50a5fb0f47def685bb1b62216575
sugizakikun <sugizakikun@gmail.com>
2024-08-30 16:00:55 +0900

    profile_imageのcomponent化

--- src/resources/views/profile.blade.php
+++ src/resources/views/profile.blade.php
@@ -12,13 +12,7 @@
             <div class="mb-5">
                 <h2 class="text-dark border-bottom">Profile</h2>
                 <div class="position-relative mb-3">
-                    <img
-                        src="{{ $user->profile_image_url ? asset($user->profile_image_url): asset('img/profile_female.png') }}"
-                        onerror="this.onerror=null; this.src='{{ asset('img/profile_female.png')  }}';"
-                        class="thumbnail"
-                        width="300"
-                        height="300"
-                    >
+                    <x-profile_image :user="$user" size=300 />
                     <div class="dropdown position-absolute color-bg-defaultcolor-fg-default px-2 py-1 left-0 bottom-0 ml-2 mb-2">
                         <button class="btn btn-light dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                             Edit
