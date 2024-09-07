f69e3017161947ab78dcffa3c13d687457ac8d83
sugizakikun <sugizakikun@gmail.com>
2024-09-07 21:37:32 +0900

    投稿を削除したら、紐づいている画像のレコード・S3オブジェクトも削除する

--- src/app/Http/Services/Posts/DeletePost.php
+++ src/app/Http/Services/Posts/DeletePost.php
@@ -3,12 +3,49 @@
 namespace App\Http\Services\Posts;
 
 use App\Models\Post;
+use App\Models\PostImage;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
 
 class DeletePost
 {
+    /**
+     * @param int $postId
+     * @return void
+     */
     public function execute(int $postId)
     {
-        (new Post())
+        DB::transaction(function () use ($postId) {
+            $this->deletePostImage($postId);
+
+            $this->deletePost($postId);
+        });
+    }
+
+    /**
+     * @param int $postId
+     * @return void
+     */
+    private function deletePostImage(int $postId):void
+    {
+        $postImages = (new PostImage())
+            ->where('post_id', $postId)
+            ->get();
+
+        foreach ($postImages as $postImage) {
+            Storage::disk('s3')->delete($postImage->image_key);
+        }
+
+        $postImages->each->delete();
+    }
+
+    /**
+     * @param int $postId
+     * @return bool
+     */
+    private function deletePost(int $postId): bool
+    {
+        return (new Post())
             ->where('id', $postId)
             ->first()
             ->delete();
