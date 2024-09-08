a6387e0096d7eb8b36686717a85c401a92d82fa7
sugizakikun <sugizakikun@gmail.com>
2024-09-08 18:19:02 +0900

    フォロー機能のロジックの作成

--- src/app/Http/Services/User/CreateFollow.php
+++ src/app/Http/Services/User/CreateFollow.php
@@ -2,7 +2,22 @@
 
 namespace App\Http\Services\User;
 
+use App\Models\Follow;
+
 class CreateFollow
 {
-
+    /**
+     * @param $following
+     * @param $followed
+     * @return Follow
+     */
+    public function execute($following, $followed): Follow
+    {
+        return (new Follow())
+            ->newQuery()
+            ->create([
+                'following' => $following,
+                'followed' => $followed,
+            ]);
+    }
 }
