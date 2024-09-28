ffa63fee5a289328beb17323c2d8c07aa21428b4
sugizakikun <sugizakikun@gmail.com>
2024-09-28 11:31:23 +0900

    ユーザー情報を受け取ったらログインする仕組みを追加

--- src/app/Http/Controllers/Auth/GoogleAuthController.php
+++ src/app/Http/Controllers/Auth/GoogleAuthController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Auth;
 use Illuminate\Http\Request;
 use App\Util\GoogleAuthClient;
 use App\Http\Controllers\Controller;
+use App\Http\Services\Auth\CreateGoogleUser;
+use Illuminate\Support\Facades\Auth;
 
 class GoogleAuthController extends Controller
 {
@@ -21,17 +23,27 @@ class GoogleAuthController extends Controller
         return redirect($url);
     }
 
-    public function handleOAuthCallback()
+    public function handleOAuthCallback(CreateGoogleUser $createGoogleUser)
     {
         $code = request()->input('code');
 
+        # 1.コールバックされたトークンを元にアクセストークンを取得
         $getTokenResponse = $this->googleAuthClient->getToken($code);
         
         if(array_key_exists('error', $getTokenResponse, )){
             $this->redirectToLogin('認証中にエラーが発生しました');
         }
 
+        # 2. 1で受け取ったアクセストークンを元にOAuthからユーザー情報を取得
         $getUserInfoResponse = $this->googleAuthClient->getUserInfo($getTokenResponse);
+        
+        # 3. 2で受け取ったユーザー情報をusersテーブルにも登録
+        $user = $createGoogleUser->execute($getUserInfoResponse);
+        
+        # 4. ユーザーを自動的にログインさせる
+        Auth::login($user);
+
+        return redirect(route('home'));
     }
 
     private function redirectToLogin($errorMessage)
